61aedc8ef717a1ba8ebc6ef9c45b096a626c9890
Ben Hall <ben@benh.codes>
2022-10-17 23:28:32 +0100

    feat(merchant): Started to add merchant logic
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/utils/party.ts
+++ src/utils/party.ts
@@ -3,14 +3,12 @@ import { Character } from "alclient";
 const partyMembers = ["Elius"];
 
 async function sendPartyInvite(bot: Character) {
-    if (bot.partyData == null) {
-        console.log(`${bot.name} is not in a party, sending party invite to ${partyMembers}...`);
-        partyMembers.forEach(async (member) => {
-            if(!bot.partyData.list.includes(member)) {
-                await bot.sendPartyInvite(member);
-            }
-        });
-    }
+    partyMembers.forEach(async (member) => {
+        if (bot.partyData == null || !bot.partyData.list.includes(member)) {
+            console.log(`${bot.name} is not in a party, sending party invite to ${partyMembers}...`);
+            await bot.sendPartyInvite(member);
+        }
+    });
 }
 
 async function acceptPartyInvite(bot: Character) {
