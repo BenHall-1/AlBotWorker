f752728b55c22a989e69e39e3c83332fa0e1ad21
Ben Hall <ben@benh.codes>
2022-10-17 23:25:22 +0100

    feat(merchant): Started to add merchant logic
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,17 @@ async function run() {
     runPrometheus();
 
     await Promise.all([AL.Game.loginJSONFile("./credentials.json"), AL.Game.getGData()]);
-    await AL.Pathfinder.prepare(AL.Game.G)
-
-    // Mage Bot
-    let mageBot = await AL.Game.startMage("Elius", "EU", "II");
-    console.log(`Logged in as ${mageBot.name}!`);
-    await runMage(mageBot);
+    await AL.Pathfinder.prepare(AL.Game.G);
 
     // Merchant Bot
     let merchantBot = await AL.Game.startMerchant("Iqium", "EU", "II");
     console.log(`Logged in as ${merchantBot.name}!`);
     await runMerchant(merchantBot);
+
+    // Mage Bot
+    let mageBot = await AL.Game.startMage("Elius", "EU", "II");
+    console.log(`Logged in as ${mageBot.name}!`);
+    await runMage(mageBot);
 
     // Update stats every 5 seconds
     setInterval(() => {
