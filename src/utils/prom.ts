888248e66abbff4147a6a5497d664306568e6d7a
Ben Hall <ben@benh.codes>
2022-10-18 18:03:32 +0100

    chore(prom): Add more statistics
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/utils/prom.ts
+++ src/utils/prom.ts
@@ -6,19 +6,31 @@ const register = new client.Registry();
 const levelGauge = new client.Gauge({
     name: 'level',
     help: 'Levels',
-    labelNames: ['character'],
+    labelNames: ['character', 'character_type'],
     registers: [register]
 });
 const xpGauge = new client.Gauge({
     name: 'xp',
     help: 'Total XP for Character',
-    labelNames: ['character'],
+    labelNames: ['character', 'character_type'],
     registers: [register]
 });
 const goldGauge = new client.Gauge({
     name: 'gold',
     help: 'Total Gold for Character',
-    labelNames: ['character'],
+    labelNames: ['character', 'character_type'],
+    registers: [register]
+});
+const healthGauge = new client.Gauge({
+    name: 'health',
+    help: 'Health for Character',
+    labelNames: ['character', 'character_type'],
+    registers: [register]
+});
+const maxHealthGauge = new client.Gauge({
+    name: 'max_health',
+    help: 'Max Health for Character',
+    labelNames: ['character', 'character_type'],
     registers: [register]
 });
 
@@ -41,9 +53,12 @@ async function run() {
 }
 
 async function updateStats(bot: Character) {
-    levelGauge.set({character: bot.name}, bot.level);
-    xpGauge.set({character: bot.name}, bot.xp);
-    goldGauge.set({character: bot.name}, bot.gold);
+    const labels = {character: bot.name, character_type: bot.ctype};
+    levelGauge.labels(labels).set(bot.level);
+    xpGauge.labels(labels).set(bot.xp);
+    goldGauge.labels(labels).set(bot.gold);
+    healthGauge.labels(labels).set(bot.hp);
+    maxHealthGauge.labels(labels).set(bot.max_hp);
 }
 
 
